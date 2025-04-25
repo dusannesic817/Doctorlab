3eb09a9b9b9be9c66a49b7e47c70ca7cfe857fec
dusannesic817 <dusannesic28@gmail.com>
2025-04-25 20:18:36 +0200

    show_appointment

--- twig-cache/5a/5a1e804aa36ed4012c51dd3e3f8b52e7.php
+++ twig-cache/5a/5a1e804aa36ed4012c51dd3e3f8b52e7.php
@@ -65,22 +65,50 @@ class __TwigTemplate_55b0bdf23fe62b2b04abf0597d27e7c7 extends Template
         // line 16
         $context['_parent'] = $context;
         $context['_seq'] = twig_ensure_traversable(($context["schedules"] ?? null));
+        $context['loop'] = [
+          'parent' => $context['_parent'],
+          'index0' => 0,
+          'index'  => 1,
+          'first'  => true,
+        ];
+        if (is_array($context['_seq']) || (is_object($context['_seq']) && $context['_seq'] instanceof \Countable)) {
+            $length = count($context['_seq']);
+            $context['loop']['revindex0'] = $length - 1;
+            $context['loop']['revindex'] = $length;
+            $context['loop']['length'] = $length;
+            $context['loop']['last'] = 1 === $length;
+        }
         foreach ($context['_seq'] as $context["_key"] => $context["schedule"]) {
             // line 17
-            echo "          <button type=\"button\" class=\"list-group-item list-group-item-action light-blue border-0\"
-            style=\"color: #0d6efd !important;\" onclick=\"toggleCards()\">
+            echo "          <button type=\"button\"
+                  class=\"list-group-item list-group-item-action light-blue border-0\"
+                  style=\"color: #0d6efd !important;\"
+                  onclick=\"toggleCards('card";
+            // line 20
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["loop"], "index", [], "any", false, false, false, 20), "html", null, true);
+            echo "')\">
+
             <b>";
-            // line 19
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["schedule"], "caregiver_data", [], "any", false, false, false, 19), "html", null, true);
+            // line 22
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["schedule"], "caregiver_data", [], "any", false, false, false, 22), "html", null, true);
             echo "</b>
           </button>
-          ";
+        ";
+            ++$context['loop']['index0'];
+            ++$context['loop']['index'];
+            $context['loop']['first'] = false;
+            if (isset($context['loop']['length'])) {
+                --$context['loop']['revindex0'];
+                --$context['loop']['revindex'];
+                $context['loop']['last'] = 0 === $context['loop']['revindex0'];
+            }
         }
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['schedule'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
-        // line 22
-        echo "        </div>
+        // line 25
+        echo "        
+        </div>
       </div>
       <div class=\"col-lg-9 d-flex align-items-center justify-content-center\">
         <div class=\"card-container\">
@@ -88,7 +116,7 @@ class __TwigTemplate_55b0bdf23fe62b2b04abf0597d27e7c7 extends Template
           <!-- Prva kartica (početno vidljiva) -->
           <div id=\"defaultCard\" class=\"card card-my-information text-center border-0 light-blue\">
             <img src=\"";
-        // line 29
+        // line 33
         echo twig_escape_filter($this->env, ($context["base_url"] ?? null), "html", null, true);
         echo "/assets/images/calendar_transparent.png\" style=\"width: 12rem; height:10rem;\"
               class=\"card-img-top mx-auto d-block\" alt=\"...\">
@@ -100,41 +128,58 @@ class __TwigTemplate_55b0bdf23fe62b2b04abf0597d27e7c7 extends Template
           </div>
 
           <!-- Druga kartica (prikazuje se kada kliknes dugme) -->
+          
           ";
-        // line 39
+        // line 44
         $context['_parent'] = $context;
         $context['_seq'] = twig_ensure_traversable(($context["schedules"] ?? null));
+        $context['loop'] = [
+          'parent' => $context['_parent'],
+          'index0' => 0,
+          'index'  => 1,
+          'first'  => true,
+        ];
+        if (is_array($context['_seq']) || (is_object($context['_seq']) && $context['_seq'] instanceof \Countable)) {
+            $length = count($context['_seq']);
+            $context['loop']['revindex0'] = $length - 1;
+            $context['loop']['revindex'] = $length;
+            $context['loop']['length'] = $length;
+            $context['loop']['last'] = 1 === $length;
+        }
         foreach ($context['_seq'] as $context["_key"] => $context["schedule"]) {
-            // line 40
-            echo "          <div id=\"secondCard\" class=\"card  card-my-information hidden mb-5\" style=\"width: 600px; height:320px\">
+            echo "           
+          <div id=\"card";
+            // line 45
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["loop"], "index", [], "any", false, false, false, 45), "html", null, true);
+            echo "\" class=\"card card-my-information hidden mb-5\" style=\"width: 600px; height:320px\">
             <header class=\"pb-1 pt-3 rounded-top\" style=\"background-color: #01264d\">
               <p class=\"ms-3\" style=\"color:white !important\"> <b><i class=\"bi bi-calendar2-event me-1\"></i> ";
-            // line 42
-            echo twig_escape_filter($this->env, twig_date_format_filter($this->env, twig_get_attribute($this->env, $this->source, $context["schedule"], "appointment_date", [], "any", false, false, false, 42), "F j, l"), "html", null, true);
+            // line 47
+            echo twig_escape_filter($this->env, twig_date_format_filter($this->env, twig_get_attribute($this->env, $this->source, $context["schedule"], "appointment_date", [], "any", false, false, false, 47), "F j, l"), "html", null, true);
             echo "
                   <span class=\"ms-4\"><i class=\"bi bi-clock-fill\"></i> ";
-            // line 43
-            echo twig_escape_filter($this->env, twig_date_format_filter($this->env, twig_get_attribute($this->env, $this->source, $context["schedule"], "start_time", [], "any", false, false, false, 43), "H:i"), "html", null, true);
+            // line 48
+            echo twig_escape_filter($this->env, twig_date_format_filter($this->env, twig_get_attribute($this->env, $this->source, $context["schedule"], "start_time", [], "any", false, false, false, 48), "H:i"), "html", null, true);
             echo "</span></b></p>
             </header>
            
             <div class=\"card-body\">
               <div class=\"d-flex flex-row mb-3\">
                 <img src=\"";
-            // line 48
+            // line 53
             echo twig_escape_filter($this->env, ($context["base_url"] ?? null), "html", null, true);
             echo "/assets/uploads/images/";
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["schedule"], "photo", [], "any", false, false, false, 48), "html", null, true);
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["schedule"], "photo", [], "any", false, false, false, 53), "html", null, true);
             echo "\" class=\"rounded-circle ms-3 mt-3\" alt=\"...\"
                   style=\"width: 65px; height: 65px; object-fit: cover;\">
                 <div class=\"p-2 ms-2 mt-3\">
                   <b class=\"teget\"> Dr. ";
-            // line 51
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["schedule"], "doctor_name", [], "any", false, false, false, 51), "html", null, true);
+            // line 56
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["schedule"], "doctor_name", [], "any", false, false, false, 56), "html", null, true);
             echo " ";
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["schedule"], "doctor_surname", [], "any", false, false, false, 51), "html", null, true);
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["schedule"], "doctor_surname", [], "any", false, false, false, 56), "html", null, true);
             echo "</b><br> <small>";
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, $context["schedule"], "docotor", [], "any", false, false, false, 51), "title", [], "any", false, false, false, 51), "html", null, true);
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, $context["schedule"], "docotor", [], "any", false, false, false, 56), "title", [], "any", false, false, false, 56), "html", null, true);
             echo "</small>
                 </div>
               </div>
@@ -143,18 +188,26 @@ class __TwigTemplate_55b0bdf23fe62b2b04abf0597d27e7c7 extends Template
               <small>Jagodina</small><br>
               <p><small>9 Rue Yvarm, 34562 Jagodina</small></p>
               <small>";
-            // line 58
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["schedule"], "caregiver_data", [], "any", false, false, false, 58), "html", null, true);
+            // line 63
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["schedule"], "caregiver_data", [], "any", false, false, false, 63), "html", null, true);
             echo "</small>
             </div>
             
           </div>
           ";
+            ++$context['loop']['index0'];
+            ++$context['loop']['index'];
+            $context['loop']['first'] = false;
+            if (isset($context['loop']['length'])) {
+                --$context['loop']['revindex0'];
+                --$context['loop']['revindex'];
+                $context['loop']['last'] = 0 === $context['loop']['revindex0'];
+            }
         }
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['schedule'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
-        // line 63
+        // line 68
         echo "        </div>
       </div>
     </div>
@@ -174,15 +227,8 @@ class __TwigTemplate_55b0bdf23fe62b2b04abf0597d27e7c7 extends Template
     integrity=\"sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz\"
     crossorigin=\"anonymous\"></script>
   <script>
-    function toggleCards() {
-      document.getElementById(\"defaultCard\").classList.add(\"hidden\");
-      document.getElementById(\"secondCard\").classList.remove(\"hidden\");
-    }
-
-    window.onload = function () {
-      document.getElementById(\"defaultCard\").classList.remove(\"hidden\");
-      document.getElementById(\"secondCard\").classList.add(\"hidden\");
-    };
+   
+    
   </script>
 </body>
 
@@ -201,7 +247,7 @@ class __TwigTemplate_55b0bdf23fe62b2b04abf0597d27e7c7 extends Template
 
     public function getDebugInfo()
     {
-        return array (  158 => 63,  147 => 58,  133 => 51,  125 => 48,  117 => 43,  113 => 42,  109 => 40,  105 => 39,  92 => 29,  83 => 22,  74 => 19,  70 => 17,  66 => 16,  55 => 7,  53 => 6,  50 => 5,  46 => 4,  35 => 1,);
+        return array (  211 => 68,  192 => 63,  178 => 56,  170 => 53,  162 => 48,  158 => 47,  153 => 45,  134 => 44,  120 => 33,  110 => 25,  93 => 22,  88 => 20,  83 => 17,  66 => 16,  55 => 7,  53 => 6,  50 => 5,  46 => 4,  35 => 1,);
     }
 
     public function getSourceContext()
@@ -221,12 +267,16 @@ class __TwigTemplate_55b0bdf23fe62b2b04abf0597d27e7c7 extends Template
         <p class=\"mt-4\"><b>Appointment History</b></p>
         <hr>
         <div class=\"list-group border-0\">
-          {% for schedule in schedules%}
-          <button type=\"button\" class=\"list-group-item list-group-item-action light-blue border-0\"
-            style=\"color: #0d6efd !important;\" onclick=\"toggleCards()\">
-            <b>{{schedule.caregiver_data}}</b>
+          {% for schedule in schedules %}
+          <button type=\"button\"
+                  class=\"list-group-item list-group-item-action light-blue border-0\"
+                  style=\"color: #0d6efd !important;\"
+                  onclick=\"toggleCards('card{{ loop.index }}')\">
+
+            <b>{{ schedule.caregiver_data }}</b>
           </button>
-          {%endfor%}
+        {% endfor %}
+        
         </div>
       </div>
       <div class=\"col-lg-9 d-flex align-items-center justify-content-center\">
@@ -244,8 +294,9 @@ class __TwigTemplate_55b0bdf23fe62b2b04abf0597d27e7c7 extends Template
           </div>
 
           <!-- Druga kartica (prikazuje se kada kliknes dugme) -->
-          {%for schedule in schedules%}
-          <div id=\"secondCard\" class=\"card  card-my-information hidden mb-5\" style=\"width: 600px; height:320px\">
+          
+          {%for schedule in schedules%}           
+          <div id=\"card{{ loop.index }}\" class=\"card card-my-information hidden mb-5\" style=\"width: 600px; height:320px\">
             <header class=\"pb-1 pt-3 rounded-top\" style=\"background-color: #01264d\">
               <p class=\"ms-3\" style=\"color:white !important\"> <b><i class=\"bi bi-calendar2-event me-1\"></i> {{schedule.appointment_date|date(\"F j, l\")}}
                   <span class=\"ms-4\"><i class=\"bi bi-clock-fill\"></i> {{schedule.start_time|date(\"H:i\")}}</span></b></p>
@@ -287,15 +338,8 @@ class __TwigTemplate_55b0bdf23fe62b2b04abf0597d27e7c7 extends Template
     integrity=\"sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz\"
     crossorigin=\"anonymous\"></script>
   <script>
-    function toggleCards() {
-      document.getElementById(\"defaultCard\").classList.add(\"hidden\");
-      document.getElementById(\"secondCard\").classList.remove(\"hidden\");
-    }
-
-    window.onload = function () {
-      document.getElementById(\"defaultCard\").classList.remove(\"hidden\");
-      document.getElementById(\"secondCard\").classList.add(\"hidden\");
-    };
+   
+    
   </script>
 </body>
 
